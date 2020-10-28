ada348ac417be7b5277778a1fda1477b145096ba
Dmitry <dslonkov@bk.ru>
2020-10-28 18:16:54 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.message').on('click', function () {
+  $('.message__forTeamLead').addClass('active')
+})
+
+$(".message1").on("click", function () {
+  $(".message__forTeamLead").removeClass("active");
+});
